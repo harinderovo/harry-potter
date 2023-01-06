18a8f80fae2a5691955fec5b70cbdf50bada2998
Harinder Singh <harmiglany@yahoo.com>
2023-01-05 23:31:25 -0800

    added some css styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,8 @@ function NavBar() {
 
   return (
     <nav className="navbar">
-      <div className="nav-wrapper">
-        <ul>
-            <Link to="/">Home</Link> <br />
-            <Link to="/characters/new">Character Form</Link> <br />
-        </ul>
-      </div>
+        <div className="nav-wrapper"><Link to="/">Home</Link></div>
+        <div className="nav-wrapper"><Link to="/characters/new">Character Form</Link></div>
     </nav>
   )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,6 @@ function Search({search, setSearch}) {
   return (
     <div className='search ui large fluid icon input'>
         <input value={search} type="text" placeholder="Search Characters" onChange={(e) => setSearch(e.target.value)} />
-        <i className="circular search link icon"></i>
     </div>
   );
 }
